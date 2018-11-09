f702094543cd14fffbf84da5eebfa4d384abd8a8
user148425 <user148425@87-237-62-16.rdns.melbourne.co.uk>
2018-11-09 22:50:59 +0000

    Initial Commit

---
+++ NewsApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApi
+//
+//  Created by user148425 on 11/9/18.
+//  Copyright © 2018 user148425. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
