504f1c4c1cc41c68c9bd8167b213b41d261cad46
user148425 <user148425@eu616.local>
2018-11-10 13:41:27 +0000

    install Moya and Alomofire via Pod

---
+++ NewsApi/commons/Globals.swift
@@ -0,0 +1,18 @@
+//
+//  Globals.swift
+//  NewsApi
+//
+//  Created by user148425 on 11/10/18.
+//  Copyright © 2018 user148425. All rights reserved.
+//
+
+import Foundation
+public  class Globals {
+    
+    static private let NewsKey = "46af9de4e3da431985c96a7d2c32a229"
+
+    public static let BaseUrl = "http://96.126.98.174"
+    
+    
+    
+}
